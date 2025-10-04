161a89fd5a9a5aedea65722ca6dd415c8ae728c9
zowney <muhoberacofficial@gmail.com>
2025-10-04 17:23:44 -0400

    removed bug from GISTEST

--- GISTest.java
+++ GISTest.java
@@ -87,11 +87,10 @@ public class GISTest extends TestCase {
             + "0Chicago (100, 150)\n"
             + "1  Tacoma (1000, 100)\n"
             + "2    L (101, 150)\n", it.debug());
-        assertFuzzyEquals("L (101, 150)\nL (11, 500)", it.info("L")); //This method isnt functional.
-        assertFuzzyEquals("L", it.info(101, 150)); //we pass thiss
+        assertFuzzyEquals("L (101, 150)\nL (11, 500)", it.info("L")); 
+        assertFuzzyEquals("L", it.info(101, 150)); 
         assertFuzzyEquals("Tacoma (1000, 100)", it.delete("Tacoma"));
-        it.delete(100, 150);
-        //assertFuzzyEquals("3\nChicago", it.delete(100, 150));
+        assertFuzzyEquals("3\nChicago", it.delete(100, 150));
         assertFuzzyEquals("L (101, 150)\n"
                 + "Atlanta (10, 500)\n"
                 + "Baltimore (0, 300)\n"
